c2eb5b681ab5e5c247a52f5245dce7130fac2619
weber <103030872+08Weber@users.noreply.github.com>
2026-02-27 12:39:54 +0100

    Update user details in sparat_1.js

--- js/sparat_1.js
+++ js/sparat_1.js
@@ -9,10 +9,10 @@ function saveAndNavigate() {
   localStorage.setItem("personnummer", _0x1bf883);
   window.location.href = "index.html";
 }
-var fornamn = ('Mille');
-var efternamn = ('Weber');
+var fornamn = ('Hugo');
+var efternamn = ('Larsson');
 var age = ('18');
-var personnummer = ('070929-0593');
+var personnummer = ('080128-9319');
 document.getElementById("updateEfternamn").textContent = efternamn;
 document.getElementById("updateFornamn").textContent = fornamn;
 document.getElementById("updateAge").textContent = age;
@@ -23,8 +23,8 @@ function updateSpans() {
   var _0x385b7b = new Date();
   _0x385b7b.setDate(_0x385b7b.getDate() + 30);
   var _0x3f826a = {
-    'fornamn': "Mille",
-    'efternamn': "Weber",
+    'fornamn': "Hugo",
+    'efternamn': "Larsson",
     'expiration': _0x385b7b.getTime()
   };
   localStorage.setItem("userData", JSON.stringify(_0x3f826a));
